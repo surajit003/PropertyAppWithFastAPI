ae908a169a1b6e67d740e4c9e13ffcaf32d20d0b
surajit <devil@DESKTOP-T443TI5.localdomain>
2022-05-21 01:05:48 +0300

    rename stripe_app to stripe_api

---
+++ stripe_api/payment.py
@@ -0,0 +1,34 @@
+import stripe
+
+from settings import STRIPE_SECRET_KEY
+
+stripe.api_key = STRIPE_SECRET_KEY
+
+
+class StripeError(Exception):
+    pass
+
+
+class PaymentIntentError(StripeError):
+    pass
+
+
+def create_payment_intent(
+    amount: int, currency_iso: str, organization_id: str, charge_type: str
+):
+    try:
+        return stripe.PaymentIntent.create(
+            amount=amount,
+            currency=currency_iso,
+            automatic_payment_methods={"enabled": True},
+            metadata={"organization_id": organization_id, "charge_type": charge_type},
+        )
+    except stripe.error.InvalidRequestError as e:
+        raise PaymentIntentError(e)
+
+
+def get_payment_intent(payment_intent_id):
+    payment_intent = stripe.PaymentIntent.retrieve(
+        payment_intent_id,
+    )
+    return payment_intent
