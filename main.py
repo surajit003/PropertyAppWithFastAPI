9a7bd0db0913101a589898326a2c828b84a75ea5
surajit <devil@DESKTOP-T443TI5.localdomain>
2022-05-21 15:38:40 +0300

    move contact api endpoint to contact route

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
 from crud.company import CompanyExistException
-from crud.contact import ContactExistException
 from dependencies.dependencies import get_db
 from schemas import schema
 from hubspot_api import utils
 from crud import company as _company
-from crud import contact as _contact
 from routes import email
+from routes import contact
 
 app = FastAPI()
 app.include_router(email.router)
-
+app.include_router(contact.router)
 templates = Jinja2Templates(directory="templates")
 
 
@@ -25,24 +24,6 @@ def read_root(request: Request):
     return templates.TemplateResponse(
         "charge.html", {"request": request, "name": "surajit"}
     )
-
-
-@app.post("/contacts")
-def create_contact(contact: schema.CreateContact, db: Session = Depends(get_db)):
-    db_contact = None
-    hubspot_contact = None
-    try:
-        org_name = contact.company_name
-        db_org = _company.filter_company_by_name(db, org_name)
-        if db_org:
-            org = db_org.all()[0]
-            db_contact = _contact.create_contact(db, contact, org.id)
-            hubspot_contact = utils.create_contact(data=contact.dict())
-    except (utils.ContactException, ContactExistException) as exc:
-        if isinstance(exc, utils.ContactException) and db_contact:
-            _contact.delete_contact(db, db_contact.id)
-        raise HTTPException(status_code=200, detail=str(exc))
-    return hubspot_contact
 
 
 @app.post("/companies")
@@ -65,12 +46,3 @@ def get_company(company_name):
     except utils.CompanyException as exc:
         raise HTTPException(status_code=200, detail=str(exc))
     return company
-
-
-@app.get("/contact/{email}/")
-def get_contact(email):
-    try:
-        contact = utils.get_contact_by_email(email)
-    except utils.ContactException as exc:
-        raise HTTPException(status_code=200, detail=str(exc))
-    return contact
--- routes/contact.py
+++ routes/contact.py
@@ -1,1 +1,43 @@
-# TODO - Move contact routes from main.py to here
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+from crud.contact import ContactExistException
+from crud import contact as _contact
+from schemas import schema
+from hubspot_api import utils
+from dependencies.dependencies import get_db
+from crud import company as _company
+
+router = APIRouter(
+    tags=["contact"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.post("/contacts/")
+def create_contact(contact: schema.CreateContact, db: Session = Depends(get_db)):
+    db_contact = None
+    hubspot_contact = None
+    try:
+        org_name = contact.company_name
+        db_org = _company.filter_company_by_name(db, org_name)
+        if db_org:
+            org = db_org.all()[0]
+            db_contact = _contact.create_contact(db, contact, org.id)
+            hubspot_contact = utils.create_contact(data=contact.dict())
+    except (utils.ContactException, ContactExistException) as exc:
+        if isinstance(exc, utils.ContactException) and db_contact:
+            _contact.delete_contact(db, db_contact.id)
+        raise HTTPException(status_code=200, detail=str(exc))
+    return hubspot_contact
+
+
+@router.get("/contact/{email}/")
+def get_contact(email):
+    try:
+        contact = utils.get_contact_by_email(email)
+    except utils.ContactException as exc:
+        raise HTTPException(status_code=200, detail=str(exc))
+    return contact
