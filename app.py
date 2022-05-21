d5933b41bb7113275074244690514f97bf4603eb
surajit <devil@DESKTOP-T443TI5.localdomain>
2022-05-21 15:44:32 +0300

    move company endpoint to company routes

---
+++ app.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+from crud.company import CompanyExistException
+from dependencies.dependencies import get_db
+from schemas import schema
+from hubspot_api import utils
+from crud import company as _company
+from routes import email
+from routes import contact
+from routes import company
+
+app = FastAPI()
+app.include_router(email.router)
+app.include_router(contact.router)
+app.include_router(company.router)
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+def read_root(request: Request):
+    return templates.TemplateResponse(
+        "charge.html", {"request": request, "name": "surajit"}
+    )
--- routes/company.py
+++ routes/company.py
@@ -1,1 +1,37 @@
-# TODO - Move company routes from main.py into this
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+from crud.company import CompanyExistException
+from schemas import schema
+from hubspot_api import utils
+from dependencies.dependencies import get_db
+from crud import company as _company
+
+router = APIRouter(
+    tags=["company"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.post("/companies/")
+def create_company(company: schema.CreateCompany, db: Session = Depends(get_db)):
+    db_company = None
+    try:
+        db_company = _company.create_company(db, company)
+        company = utils.create_company(data=company.dict())
+    except (utils.ContactException, CompanyExistException) as exc:
+        if isinstance(exc, utils.ContactException) and db_company:
+            _company.delete_company(db, db_company.id)
+        raise HTTPException(status_code=200, detail=str(exc))
+    return company
+
+
+@router.get("/company/{company_name}/")
+def get_company(company_name):
+    try:
+        company = utils.get_company_by_name(company_name)
+    except utils.CompanyException as exc:
+        raise HTTPException(status_code=200, detail=str(exc))
+    return company
