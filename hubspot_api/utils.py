f1e2e1530530033203fb8d49f0befe5f259e336c
surajit <devil@DESKTOP-T443TI5.localdomain>
2022-05-22 15:28:48 +0300

    modify requirements

--- hubspot_api/utils.py
+++ hubspot_api/utils.py
@@ -103,7 +103,12 @@ def get_contact_by_email(email):
         contact = api_client.crm.contacts.search_api.do_search(request)
         if contact.results:
             contact_detail = contact.results[0]
-            return {"id": contact_detail.id}
+            contact_properties = contact_detail.properties
+            return {"id": contact_detail.id,
+                    "email": contact_properties["email"],
+                    "firstname": contact_properties["firstname"],
+                    "lastname": contact_properties["lastname"],
+                    }
         else:
             return {}
     except contacts.exceptions.ApiException as e:
