2413133d8651e03ec4b62ce135203229c828e426
surajit <devil@DESKTOP-T443TI5.localdomain>
2022-05-23 21:26:37 +0300

    modify app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
@@ -5,6 +7,7 @@ from fastapi.templating import Jinja2Templates
 from routes.v1 import email
 from routes.v1 import contact
 from routes.v1 import company
+from settings import LOG_FILE
 
 app = FastAPI()
 app.include_router(email.router)
@@ -19,3 +22,14 @@ def home(request: Request):
     return templates.TemplateResponse(
         "charge.html", {"request": request, "message": "Welcome to Property Management API"}
     )
+
+
+@app.on_event("startup")
+async def startup_event():
+    try:
+        os.makedirs(LOG_FILE)
+        if not os.path.exists(LOG_FILE):
+            os.mknod(LOG_FILE)
+    except FileExistsError:
+        # directory already exists
+        pass
