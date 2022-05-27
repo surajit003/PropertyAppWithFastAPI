a9fe111138a01d2db62f8f479e0f148d968f9850
surajit <devil@DESKTOP-T443TI5.localdomain>
2022-05-27 11:55:32 +0300

    log integration with dynamodb

---
+++ aws/services/dynamo_db/logs.py
@@ -0,0 +1,26 @@
+import json
+
+import boto3
+import botocore
+
+import settings
+
+dynamodb = boto3.resource('dynamodb', aws_access_key_id=settings.AWS_SECRET_KEY_ID,
+                          aws_secret_access_key=settings.AWS_SECRET_KEY,
+                          region_name=settings.AWS_REGION)
+
+
+class BotoClientException(Exception):
+    pass
+
+
+async def create_log(data):
+    try:
+        table = dynamodb.Table('Log')
+        trans = {'log_id': data['log_id'],
+                 'request_ip': data['request_ip'],
+                 'message': json.dumps(data['message'])}
+        return table.put_item(Item=trans)
+    except botocore.exceptions.ClientError as e:
+        raise BotoClientException(e)
+
