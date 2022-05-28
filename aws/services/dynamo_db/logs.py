121ab23fa65290679054647838988030d25a26a5
surajit <devil@DESKTOP-T443TI5.localdomain>
2022-05-28 12:26:21 +0300

    run black

--- aws/services/dynamo_db/logs.py
+++ aws/services/dynamo_db/logs.py
@@ -5,9 +5,12 @@ import botocore
 
 import settings
 
-dynamodb = boto3.resource('dynamodb', aws_access_key_id=settings.AWS_SECRET_KEY_ID,
-                          aws_secret_access_key=settings.AWS_SECRET_KEY,
-                          region_name=settings.AWS_REGION)
+dynamodb = boto3.resource(
+    "dynamodb",
+    aws_access_key_id=settings.AWS_SECRET_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_KEY,
+    region_name=settings.AWS_REGION,
+)
 
 
 class BotoClientException(Exception):
@@ -16,11 +19,12 @@ class BotoClientException(Exception):
 
 async def create_log(data):
     try:
-        table = dynamodb.Table('Log')
-        trans = {'log_id': data['log_id'],
-                 'request_ip': data['request_ip'],
-                 'message': json.dumps(data['message'])}
+        table = dynamodb.Table("Log")
+        trans = {
+            "log_id": data["log_id"],
+            "request_ip": data["request_ip"],
+            "message": json.dumps(data["message"]),
+        }
         return table.put_item(Item=trans)
     except botocore.exceptions.ClientError as e:
         raise BotoClientException(e)
-
